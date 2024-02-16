b42a7115d0ab8282d60058a71d6d803d4eaf7a71
HoangDang <hoangdang91768@gmail.com>
2024-02-16 14:13:45 -0600

    working

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -13,8 +13,19 @@ fizzbuzz(16);
 */
 
 const fizzbuzz = num => {  
-  
+  const output = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) output.push('fizzbuzz')
+    else if (i % 5 === 0) output.push('buzz')
+    else if (i % 3 === 0) output.push('fizz')
+    else output.push(i)
+  }
+
+  return output;
 };
+
+console.log(fizzbuzz(16));
 
 /*
 
