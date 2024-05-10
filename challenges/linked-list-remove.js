5872d23840cf9ce733d4a7547016ad5bdff82f7c
HoangDang <hoangdang91768@gmail.com>
2024-05-09 19:47:24 -0700

    working

--- challenges/linked-list-remove.js
+++ challenges/linked-list-remove.js
@@ -28,10 +28,51 @@ function Node(val) {
   this.next = null;
 }
 
-const linkedListRemove = (ll, val) => {
+// Create the linked list instance
+let myList = new LinkedList();
 
+// Create nodes
+const node1 = new Node('a');
+const node2 = new Node('b');
+const node3 = new Node('c');
+const node4 = new Node('b');
+const node5 = new Node('d');
+
+// Link nodes
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
+// Set the head of the linked list
+myList.head = node1;
+
+const linkedListRemove = (ll, val) => {
+  if (!ll || !ll.head) return;
+
+  let current = ll.head;
+  let prev = null;
+
+  // if the removed item is the head
+  if (current.val === val) {
+    ll.head = current.next;
+    return ll;
+  }
+
+  while (current !== null && current.val !== val) {
+    prev = current;
+    current = current.next;
+  }
+
+  // If the node was found and is not the last node
+  if (current !== null) {
+    prev.next = current.next;  // Remove the current node by updating the link
+  }
+
+  return ll;
 };
 
+console.log(linkedListRemove(myList, 'e'))
 
 /*
 Extension: 
